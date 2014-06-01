cce2ff1d107fa44eeba2a1765049332c74dd87d5
Ivo Dlouhy <xdlouh05@stud.fit.vutbr.cz>
2014-06-01 22:40:57 +0200

    Import manual.

---
+++ doc/manual/src/makefile.bat
@@ -0,0 +1,36 @@
+# clean
+del *.bbl
+del *.blg
+del *.mtc*
+del *.mlf*
+del *.mlt*
+del *.maf
+del *.aux
+del *.out
+del *.toc
+del *.lof
+del *.lot
+del *.log
+del *.snm
+del *.nav
+
+# build umllint-manual
+del umllint-manual.pdf
+pdflatex umllint-manual
+pdflatex umllint-manual
+
+# clean
+del *.bbl
+del *.blg
+del *.mtc*
+del *.mlf*
+del *.mlt*
+del *.maf
+del *.aux
+del *.out
+del *.toc
+del *.lof
+del *.lot
+del *.log
+del *.snm
+del *.nav
